958cd731ea4dc9d7dec4792279b48685a926f941
Prathamesh Kowarkar <hello@prathamesh.xyz>
2021-05-09 20:52:56 +0530

    Show states in a picker

--- VaxWatch/Views/BrowseByDistrictView.swift
+++ VaxWatch/Views/BrowseByDistrictView.swift
@@ -3,16 +3,28 @@ import SwiftUI
 struct BrowseByDistrictView: View {
 
     @State var date = Date()
+    @StateObject var appModel = DistrictsViewModel()
+    @State var selectedStateIndex = 0
 
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Details")) {
                     DatePicker("Vaccination Date", selection: $date, in: Date()..., displayedComponents: .date)
+                    if appModel.states.isEmpty {
+                        Button("Fetch States List", action: appModel.fetchStates)
+                    } else {
+                        Picker("State", selection: $selectedStateIndex) {
+                            ForEach(appModel.states.sorted { $0.value < $1.value }, id: \.key) { state in
+                                Text(state.value)
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Browse By Districts")
         }
+        .onAppear(perform: appModel.fetchStates)
     }
 
 }
