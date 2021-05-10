bc31cf5e05396b7d776d3bfe6ec4e6eb00ff2ccf
Prathamesh Kowarkar <hello@prathamesh.xyz>
2021-05-11 00:30:30 +0530

    Scale badge padding with dynamic text sizing

--- VaxWatch/Views/Badge.swift
+++ VaxWatch/Views/Badge.swift
@@ -5,6 +5,8 @@ struct Badge: View {
     let text: String
     let foregroundColor: Color
     let backgroundColor: Color
+    @ScaledMetric var verticalPadding: CGFloat = 2
+    @ScaledMetric var horizontalPadding: CGFloat = 8
 
     var body: some View {
         Text(text)
@@ -12,7 +14,14 @@ struct Badge: View {
             .fontWeight(.bold)
             .textCase(.uppercase)
             .foregroundColor(foregroundColor)
-            .padding(EdgeInsets(top: 2, leading: 8, bottom: 2, trailing: 8))
+            .padding(
+                EdgeInsets(
+                    top: verticalPadding,
+                    leading: horizontalPadding,
+                    bottom: verticalPadding,
+                    trailing: horizontalPadding
+                )
+            )
             .background(backgroundColor)
             .clipShape(Capsule())
     }
