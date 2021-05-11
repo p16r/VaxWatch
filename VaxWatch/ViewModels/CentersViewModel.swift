d64e38a5e99a6c219312557091e035bcc14c89b9
Prathamesh Kowarkar <hello@prathamesh.xyz>
2021-05-11 13:10:19 +0530

    Nilify centers before API load

--- VaxWatch/ViewModels/CentersViewModel.swift
+++ VaxWatch/ViewModels/CentersViewModel.swift
@@ -19,6 +19,7 @@ class CentersViewModel: ObservableObject {
     }
 
     func fetchCentres(for districtIndex: Int) {
+        centers = nil
         apiClient
             .centersByDistrictPublisher(for: districtIndex)
             .receive(on: DispatchQueue.main)
@@ -27,7 +28,6 @@ class CentersViewModel: ObservableObject {
                     case .success(let centers):
                         self?.centers = centers
                     case .failure(let error):
-                        self?.centers = nil
                         self?.errorMessage = "\(error.localizedDescription)\nPlease try again later."
                 }
             }
@@ -35,6 +35,7 @@ class CentersViewModel: ObservableObject {
     }
 
     func fetchCentres(in pincode: Int) {
+        centers = nil
         apiClient
             .centersByPincodePublisher(for: pincode)
             .receive(on: DispatchQueue.main)
@@ -43,7 +44,6 @@ class CentersViewModel: ObservableObject {
                     case .success(let centers):
                         self?.centers = centers
                     case .failure(let error):
-                        self?.centers = nil
                         self?.errorMessage = "\(error.localizedDescription)\nPlease try again later."
                 }
             }
