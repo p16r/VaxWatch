d1bdf028d2bd16ab14599bc6a4c728ec84736c36
Prathamesh Kowarkar <hello@prathamesh.xyz>
2021-05-11 11:43:25 +0530

    Add pincode validation

--- VaxWatch/Views/BrowseByPincodeView.swift
+++ VaxWatch/Views/BrowseByPincodeView.swift
@@ -3,12 +3,23 @@ import SwiftUI
 struct BrowseByPincodeView: View {
 
     @State var pincodeString: String = ""
+    @State var pincodeMessage: String? = nil
+
+    private var pincode: Int? {
+        Int(pincodeString)
+    }
 
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Pincode")) {
+                Section(
+                    header: Text("Pincode"),
+                    footer: pincodeMessage.map { Text($0).foregroundColor(.red) }
+                ) {
                     TextField("110001", text: $pincodeString, onCommit: {
+                        if pincodeString.count != 6 { return pincodeMessage = "Pincode must be 6 digits long only." }
+                        if pincode == nil { return pincodeMessage = "Pincode can only contain numbers." }
+                        pincodeMessage = nil
                         print(pincodeString)
                     })
                     .keyboardType(.numbersAndPunctuation)
