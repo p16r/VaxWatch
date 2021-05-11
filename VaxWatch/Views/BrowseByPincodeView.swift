5690220e24d33e0c89dc1ff292067d2233298cb6
Prathamesh Kowarkar <hello@prathamesh.xyz>
2021-05-11 11:07:53 +0530

    Add browse by pincode view

---
+++ VaxWatch/Views/BrowseByPincodeView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct BrowseByPincodeView: View {
+
+    var body: some View {
+        Text("Pincode")
+    }
+
+}
+
+struct BrowseByPincodeView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        BrowseByPincodeView()
+    }
+
+}
