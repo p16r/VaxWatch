abdaa4abbf0079b56f3a917dc4c1bc69c929a7e6
Prathamesh Kowarkar <hello@prathamesh.xyz>
2021-05-19 19:58:05 +0530

    Add details to about view

--- VaxWatch/Views/AboutView.swift
+++ VaxWatch/Views/AboutView.swift
@@ -2,8 +2,44 @@ import SwiftUI
 
 struct AboutView: View {
 
+    private let aboutVaxWatchString = """
+    This app uses the Co-WIN Public API provided by API Setu to browse vaccination centers by district or pincode and \
+    display information and availability of vaccination slots.
+    The appointment availability data that this app fetches is not live, but from a cache that may be upto 30 minutes \
+    old. Live data is not made publicly available for third-party use.
+    """
+    private let aboutMeString = """
+    This open-source app is made by me, Prathamesh Kowarkar, an iOS app developer from Mumbai, India.
+    """
+    private let githubRepoURL = URL(string: "https://github.com/prtmshk/VaxWatch")!
+    private let githubURL = URL(string: "https://github.com/prtmshk")!
+    private let twitterURL = URL(string: "https://twitter.com/prtmshk")!
+    private let emailURL = URL(string: "mailto:prathamesh.kowarkar@gmail.com")!
+    private let privacyURL = URL(string: "https://github.com/prtmshk/VaxWatch/blob/main/PRIVACY.md")!
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("About This App")) {
+                    Text(aboutVaxWatchString)
+                }
+                Section(header: Text("This App Is Open-Source!")) {
+                    Link("View source code on GitHub", destination: githubRepoURL)
+                }
+                Section(header: Text("About Me")) {
+                    Text(aboutMeString)
+                }
+                Section(header: Text("Contact Me")) {
+                    Link("Twitter", destination: twitterURL)
+                    Link("GitHub", destination: githubURL)
+                    Link("Email", destination: emailURL)
+                }
+                Section(header: Text("VaxWatch Values Your Privacy")) {
+                    Link("Read the full policy here", destination: privacyURL)
+                }
+            }
+            .navigationTitle("About VaxWatch")
+        }
     }
 
 }
